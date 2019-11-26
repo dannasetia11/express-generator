cde873f28f2dd78fe963a322585ef3a21f664ab7
dannasetia <dannasetia@gmail.com>
2019-11-26 14:46:25 +0700

    add home

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+  res.render("index", { title: "Express" });
+});
+router.get("/home", (req, res) => {
+  res.json({ message: "Home" });
 });
 
 module.exports = router;
